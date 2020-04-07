7ed2dacc90fd37ec03f97389860858fc3f23987e
Renan-NO <re.office.25@gmail.com>
2020-04-07 19:43:54 -0300

    Update

---
+++ mobile/src/history.js
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import login from './pages/Login/index';
+
+const user = login.history;
+
+export default user;
--- mobile/src/pages/Login/styles.js
+++ mobile/src/pages/Login/styles.js
@@ -1,28 +1,124 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
     container: {
         flex: 1,
-        paddingHorizontal: 24
-    },
-    
-    actions: {
-        marginTop: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#fff',
+
     },
 
-    action: {
-        backgroundColor: '#ffaf69',
-        borderRadius: 8,
-        height: 50,
-        width: '48%',
+    containerHeader: {
+        alignItems: 'center',
         justifyContent: 'center',
-        alignItems: 'center'
+        backgroundColor: '#FFAF69',
+        borderBottomEndRadius: 20,
+        borderBottomStartRadius: 20,
+        width: '100%',
+        height: 170,
     },
 
-    actionText: {
-        color: '#fff',
+    containermiddle: {
+        paddingTop: '20%',
+        paddingHorizontal: 50,
+    },
+
+    formText: {
+        fontSize: 18,
+        alignItems: 'center',
+        color: '#000',
+        borderBottomWidth: 1,
+        borderBottomColor: '#000',
+        paddingBottom: 20,
+        paddingTop: 20,
+    },
+
+    containerBox: {
+        marginTop: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+
+    formUs: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        marginBottom: 20,
+        borderColor: '#000',
+        borderBottomWidth: 1,
+        paddingBottom: 10,
+    },
+
+    formPass: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 20,
+        marginBottom: 20,
+        borderColor: '#000',
+        borderBottomWidth: 1,
+        paddingBottom: 10,
+    },
+    containerBox: {
+
+    },
+
+    formTextUs: {
+        fontSize: 18,
+        width: '95%'
+    },
+
+    formTextPass: {
+        fontSize: 18,
+        width: '95%'
+    },
+
+    containerBox: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+    },
+
+    boxText: {
+        marginLeft: 20,
         fontSize: 15,
+    },
+
+    containerPassword: {
+        alignItems: 'center',
+        marginTop: 55,
+    },
+
+    forgetPassword: {
+        borderColor: '#000',
+        borderBottomWidth: 1,
+        fontSize: 12,
+    },
+
+    containerButton: {
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    login: {
+        backgroundColor: '#FFAF69',
+        height: 40,
+        borderRadius: 8,
+        width: '100%',
+        marginTop: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    loginText: {
+        fontSize: 22,
+        color: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
-})
+});
+
